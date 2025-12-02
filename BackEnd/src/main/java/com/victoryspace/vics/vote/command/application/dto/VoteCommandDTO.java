b6959f95ef7dda5c85ea628b976dc278b51b1032
= <tjalswhd1@naver.com>
2025-12-02 14:32:22 +0900

    feat : 투표에 카테고리 기능 생성

--- BackEnd/src/main/java/com/victoryspace/vics/vote/command/application/dto/VoteCommandDTO.java
+++ BackEnd/src/main/java/com/victoryspace/vics/vote/command/application/dto/VoteCommandDTO.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class VoteCommandDTO {
     private Integer id;
     private String title;
+    private Integer categoryId;
     private Integer memberId;
     private String content;
     private Integer challengerId;
