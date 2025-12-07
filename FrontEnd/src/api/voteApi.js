678e7cd1572b5696cf7de7f870c459bec53fe80a
= <tjalswhd1@naver.com>
2025-12-07 17:39:53 +0900

    merge : develop 브랜치 병합

--- FrontEnd/src/api/voteApi.js
+++ FrontEnd/src/api/voteApi.js
@@ -1,9 +1,5 @@
 // src/api/voteApi.js
-<<<<<<< HEAD
-import api from "@/api/http.js";
-=======
 import http from "./http";
->>>>>>> f4a43908cd0aecefe2250f30366e31089a805544
 
 const BASE_URL = "/vote";
 
